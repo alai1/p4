f31806ff8ba0736f44952dfe4d1c649b777e4b51
Aymon Fournier <ceo@invio.us>
2015-04-28 00:19:07 -0400

    Holy fuck there is the weirdest bug ever in my code. Corrupted stack or something from concatenating strings. Fixed it by putting a dummy variable between the file name and the key

--- src/init.c
+++ src/init.c
@@ -16,23 +16,6 @@
 #define FALSE 0
 #endif
 
-typedef unsigned char byte;
-void ToFile(byte *buffer, size_t len)
-{
-    FILE *f = fopen("out.txt", "w");
-    if (f == NULL)
-    {
-        fprintf(stderr, "Error opening file!\n");
-        exit(EXIT_FAILURE);
-    }
-    int i;
-    for (i = 0; i < len; i++)
-    {
-        fprintf(f, "%u", buffer[i]);
-    }
-    fclose(f);
-}
-
 int main(int argc, char *argv[]) {
 
 if ( argc != 2 ){
@@ -42,28 +25,49 @@ if ( argc != 2 ){
 
 FILE *bankFile, *atmFile;
     
-char bankFileName[255];
+char bankFileName[255] = "";
 snprintf(bankFileName, sizeof bankFileName, "%s%s", argv[1], ".bank");
 
-char atmFileName[255];
-snprintf(bankFileName, sizeof atmFileName, "%s%s", argv[1], ".atm");
+char atmFileName[255] = "";
+snprintf(atmFileName, sizeof atmFileName, "%s%s", argv[1], ".atm");
+
+char someThingIsSeverelyBrokenInMemoryDontDelete[255] = "";
+/*If you delete it, when you create unsigned key[1] and then do the random bytes function,
+you get a random byte with atmFileName appended to the end! e.g. "A0keys.atm" No matter
+what the key size it would always append atmFileName to it.
+
+I spent like 3 hours trying to figure out why this was happening and I think it has something
+to do will null pointers or something but I have no idea why its broken. Putting a variable on
+the stack between them fixed it. HOLY FUCK.
+*/
 
 if( access( bankFileName, F_OK ) != -1 || access( atmFileName, F_OK ) != -1 ) {
     printf("Error:  one of the files already exists");
     return 63;
 }
 
-//We're using EVP_aes_256_gcm
-unsigned char key[32];
-
+ //We're using EVP_aes_256_gcm
+unsigned char key[32] = "";
+printf("size of key: %d\n", sizeof key);
 if (!RAND_bytes(key, sizeof key)) {
     printf("Error creating initialization files\n");
     return 64;
 }
+printf("size of key: %d\n", sizeof key);
+printf("%c\n", key[2]);
+
+int counter = 0;
+while (counter < sizeof key)
+{
+     printf("%02X",(int)key[counter]);
+     counter++;
+}
+printf("%s\n", key);
+
 
 bankFile = fopen(bankFileName, "w");
 
-int results = fputc(key, bankFile);
+int results = fputs(key, bankFile);
 if (results == EOF) {
     printf("Error creating initialization files\n");
     return 64;
@@ -72,12 +76,14 @@ fclose(bankFile);
 
 atmFile = fopen(atmFileName, "w");
 
-results = fputc(key, atmFile);
+results = fputs(key, atmFile);
 if (results == EOF) {
     printf("Error creating initialization files\n");
     return 64;
 }
 fclose(atmFile);
+
+
 
 printf("Successfully initialized bank state\n");
 
